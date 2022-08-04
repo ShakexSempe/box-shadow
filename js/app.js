eaf0197e6c602928fb87a2917484bd4ef689561a
Ray-Shakex <bshakex@gmail.com>
2022-08-04 15:30:50 +0200

    app.js and scroll padding

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@ hamburger.addEventListener("click", () => {
     aside.classList.toggle("active");
     hamburger.classList.toggle("active");
 });
+
+const links = document.querySelectorAll('header a');
+console.log(links);
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        aside.classList.remove("active");
+    });
+})

cc47362ce5425e3b958f6294221a6bd5ff842611
Ray-Shakex <bshakex@gmail.com>
2022-08-05 11:41:44 +0200

    header intersection observer

--- js/app.js
+++ js/app.js
@@ -1,17 +1,39 @@
 const hamburger = document.querySelector('.hamburger');
 const aside = document.querySelector('.aside');
+const links = document.querySelectorAll('header a');
+console.log(links);
 
+// HAMBURGER TOGGLE
 hamburger.addEventListener("click", () => {
     aside.classList.toggle("active");
     hamburger.classList.toggle("active");
 });
 
-const links = document.querySelectorAll('header a');
-console.log(links);
-
+// REMOVE HAMBURGER ON LINK CLICK
 links.forEach(link => {
     link.addEventListener("click", () => {
         hamburger.classList.remove("active");
         aside.classList.remove("active");
     });
-})
+});
+
+// HERO INTERSECTION OBSERVER
+const header = document.querySelector('.header');
+const hero = document.querySelector('.hero');
+const heroOptions = {
+    rootMargin: '-20% 0px 0px 0px',
+}
+const heroObserver = new IntersectionObserver(
+    function( entries, heroObserver) {
+        entries.forEach( entry => {
+            if(!entry.isIntersecting) {
+                console.log("Her NOT io");
+                header.classList.remove('hero-header');
+            } else {
+                console.log("hero IS io");
+                header.classList.add('hero-header');
+            }
+        });
+    }, heroOptions
+);
+heroObserver.observe(hero);
